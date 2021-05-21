92d4554fd19c79a04bf8262ab32f6295a09fa62b
Harry Manchanda <harmanmanchanda182@gmail.com>
2021-05-21 16:35:50 +0530

    Rename title of the project

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-  baseURL: "https://my-json-server.typicode.com/IamManchanda/real-world-vue-3",
+  baseURL: "https://my-json-server.typicode.com/IamManchanda/real-world-vue3",
   withCredentials: false,
   headers: {
     Accept: "application/json",
